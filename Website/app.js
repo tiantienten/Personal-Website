e729929f7b5f7d676d8adf9890352cc8ef26eeb6
tiantienten <tianzez@andrew.cmu.edu>
2024-04-26 13:23:59 -0400

    update

---
+++ Website/app.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".texts",{
+    strings: ["COLLEGE STUDENT", "BEGINNER CIVIL ENGINEER", "RESEARCH ASSISTANT", "UX/UI Learner"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
