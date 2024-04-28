e5a825a81fcbea9d61a921065894159ccb61aeea
tiantienten <tianzez@andrew.cmu.edu>
2024-04-27 23:17:28 -0400

    1

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Type.js animation
 var typed = new Typed(".texts",{
     strings: ["COLLEGE STUDENT", "BEGINNER CIVIL ENGINEER", "RESEARCH ASSISTANT", "UX/UI Learner"],
     typeSpeed: 100,
@@ -5,3 +6,24 @@ var typed = new Typed(".texts",{
     backDelay: 1000,
     loop: true
 })
+
+//scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks  = document.querySelectorAll('header navigation a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header navigation a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+}
