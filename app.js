e9a3edf3a385d51cda21714375d9899ecc6406f3
tiantienten <tianzez@andrew.cmu.edu>
2024-04-28 02:02:18 -0400

    1

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var typed = new Typed(".texts",{
 })
 
 //scroll sections
-let sections = document.querySelectorAll('section');
-let navLinks  = document.querySelectorAll('header navigation a');
+let sections = document.querySelectorAll('.page');
+let navLinks  = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
@@ -18,12 +18,13 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height){
+        if(top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header navigation a[href*=' + id + ']').classList.add('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         }
     });
-
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
 }
